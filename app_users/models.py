c03df0012e18b9e65c6b8ee7f87672773621479a
Oybek Narzullayev <oybeknarzullaev@gmail.com>
2021-06-29 23:04:37 +0500

    '2-dars'

--- app_users/models.py
+++ app_users/models.py
@@ -3,20 +3,21 @@ from django.contrib.auth.models import User
 import os
 from django.urls import reverse
 
+
 def path_and_rename(instance, filename):
     upload_to = 'Images/'
     ext = filename.split('.')[-1]
     # get filename
     if instance.pk:
-        filename = 'User_Profile_Pictures/{}.{}'.format(instance.pk, ext)
+        filename = 'ser_Profile_Pictures/{}.{}'.format(instance.pk, ext)
     return os.path.join(upload_to, filename)
 
-class UserProfileInfo(models.Model):
 
-    #creating a relationship with user class (not inheriting)
+class UserProfileInfo(models.Model):
+    # creating a relationship with user class (not inheriting)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    #adding additional attributes
+    # adding additional attributes
     bio = models.CharField(max_length=500)
     profile_pic = models.ImageField(upload_to=path_and_rename, verbose_name="Profile Picture", blank=True)
     teacher = 'teacher'
--- app_users/views.py
+++ app_users/views.py
@@ -1,7 +1,68 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import UserForm, UserProfileInfoForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
-def index(request):
-    return HttpResponse("This is home page!")
 # Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+
+def register(request):
+
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            # user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'app_users/registration.html',
+                            {'registered':registered,
+                             'user_form':user_form,
+                             'profile_form':profile_form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("ACCOUNT IS DEACTIVATED")
+        else:
+            return HttpResponse("Please use correct id and password")
+            # return HttpResponseRedirect(reverse('register'))
+
+    else:
+        return render(request, 'app_users/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
