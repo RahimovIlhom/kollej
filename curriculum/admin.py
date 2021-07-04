4bf2313cec7bdf17cedb7a1b34bb0383d4da857a
Oybek Narzullayev <oybeknarzullaev@gmail.com>
2021-07-04 18:29:43 +0500

    4 - dars o'quv dasturi

---
+++ curriculum/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Standard, Subject, Lesson
+
+# Register your models here.
+
+
+admin.site.register(Standard)
+admin.site.register(Subject)
+admin.site.register(Lesson)
--- teaching_blog/settings.py
+++ teaching_blog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_users',
+    'curriculum',
 ]
 
 MIDDLEWARE = [
